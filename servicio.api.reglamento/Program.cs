b5381b3197d6b3f3366af4b6542506941c771a72
ckomalram <glaw14@gmail.com>
2023-04-17 11:35:20 -0500

    ++ conf program.cs + EF + services

--- servicio.api.reglamento/Program.cs
+++ servicio.api.reglamento/Program.cs
@@ -1,3 +1,7 @@
+using servicio.api.reglamento.Core.Context;
+using servicio.api.reglamento.Core.Interfaces;
+using servicio.api.reglamento.Core.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// conf ef
+builder.Services.AddSqlServer<ReglamentoContext>(builder.Configuration.GetConnectionString("cnLpf"));
+// inyectar dependencias 
+builder.Services.AddScoped<IReglamento, ReglamentoService>();
+
+
 
 var app = builder.Build();
 
